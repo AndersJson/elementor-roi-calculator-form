2d453457c3f7b895af9bd5f7b6097929d9c92661
Anders Jonsson <andersh_hotmail.com>
2020-12-29 09:19:16 +0100

    refactorized roi-range

--- widgets/roi-calculator.php
+++ widgets/roi-calculator.php
@@ -1758,7 +1758,7 @@ class ROI_Calculator_Widget extends Widget_Base {
                             <?php endif; ?>
                             </label>
                             <div class="roi-right">
-                                <div class="rangewrapper">
+                                <div class="range__wrapper">
                                     <div class="range__value" id="first-rangevalue"></div>
                                     <input id="first-range" type="range" min="0" max="10" step="1">
                                 </div>
@@ -1805,7 +1805,7 @@ class ROI_Calculator_Widget extends Widget_Base {
                                 <?php endif; ?>
                             </label>
                             <div class="roi-right">
-                                <div class="rangewrapper">
+                                <div class="range__wrapper">
                                     <div class="range__value" id="second-rangevalue"></div>
                                     <input id="second-range" type="range" min="0" max="10" step="1">
                                 </div>
@@ -1830,7 +1830,7 @@ class ROI_Calculator_Widget extends Widget_Base {
                                     <?php endif; ?>
                             </label>
                             <div class="roi-right">
-                                <div class="rangewrapper">
+                                <div class="range__wrapper">
                                     <div class="range__value" id="third-rangevalue"></div>
                                     <input id="third-range" type="range" min="0" max="10" step="1">
                                 </div>
